a92b96edb7eea3c0cd3e80940e3be765e634a9d9
axelbon <axel.bon.cc@gmail.com>
2025-05-24 12:25:06 -0600

    add pos module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CompanyModule } from './company/company.module';
 import { ProductModule } from './product/product.module';
+import { PosModule } from './pos/pos.module';
 
 @Module({
-  imports: [UserModule, CompanyModule, ProductModule],
+  imports: [UserModule, CompanyModule, ProductModule, PosModule],
   controllers: [AppController],
   providers: [AppService],
 })
