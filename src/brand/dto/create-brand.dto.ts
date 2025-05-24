c03cec184ff87741dd2c49e5bcf8ef4b1c812569
axelbon <axel.bon.cc@gmail.com>
2025-05-24 17:09:55 -0600

    add dto logic

--- src/brand/dto/create-brand.dto.ts
+++ src/brand/dto/create-brand.dto.ts
@@ -1,1 +1,21 @@
-export class CreateBrandDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateBrandDto {
+
+    @ApiProperty({example: 'Ford', description: 'Brand name'})
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @ApiProperty({example: 'Ford company', description: 'Brand description'})
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+
+    @ApiProperty({example: true, description: 'Is active', required: false})
+    @IsBoolean()
+    @IsOptional()
+    is_active?: boolean;
+
+}
