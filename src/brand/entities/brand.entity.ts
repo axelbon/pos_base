863e57b095570032f3e03098283a4d342b4ae88a
axelbon <axel.bon.cc@gmail.com>
2025-05-24 17:07:19 -0600

    add entity logic

--- src/brand/entities/brand.entity.ts
+++ src/brand/entities/brand.entity.ts
@@ -1,1 +1,25 @@
-export class Brand {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Brand {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    is_active: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at: Date;
+}
