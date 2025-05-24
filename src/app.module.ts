f337b932bf861328e1be6cda9e894c6a61747c01
axelbon <axel.bon.cc@gmail.com>
2025-05-24 12:23:51 -0600

    add product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CompanyModule } from './company/company.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [UserModule, CompanyModule],
+  imports: [UserModule, CompanyModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
