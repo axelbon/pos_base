8e404ed9260970587dfae7e86621568c8272e607
axelbon <axel.bon.cc@gmail.com>
2025-08-28 14:30:43 -0600

    Fix tests

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
 
 describe('CategoryService', () => {
   let service: CategoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [
+        CategoryService,
+        {
+          provide: getRepositoryToken(Category),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+            // agrega más métodos mockeados según use tu servicio
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
